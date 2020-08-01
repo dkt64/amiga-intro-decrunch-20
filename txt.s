2fd97ce97dccea64f301084b4cf1d17748993c95
dkt64 <b.apanasewicz@gmail.com>
2020-08-01 19:01:58 +0200

    alpha

---
+++ txt.s
@@ -0,0 +1,21 @@
+
+txt_full:
+	dc.b	'samar productions proudly presents first amiga production       '
+	dc.b	'released at decrunch demoparty in wroclaw in 2020      '
+	dc.b	'msx by jammer, gfx by isildur, code by dkt        '
+	dc.b	'unfortunately intro requires faster machine than ocs, but it was a great time '
+	dc.b	'for me (dkt) to learn amiga during last two weeks. today i added dycp scroller and '
+	dc.b	'run out of raster time, but its too late now for drastic optimisation. '
+	dc.b	'beside this you will notice a bug on left side of scroller, because i didnt have time to code a mask.'
+	dc.b	'see ya all in next amiga release from us. '
+	dc.b	'    rise your beers party people and shout amigaaaaaaa......                 greetings to '
+	dc.b	"nectarine, rabenauge, rr, t.love, dreamweb, agenda, retroguru, "
+	dc.b	"aberration creations, futuris, apidya, anadune, altair, bonzai, odbyte design, madwizards, floppy, dma, appendix, aeon, zsd, "
+	dc.b	"nah-kolor, zorandom, elysium, inflexion, decree, lepsi de, lnx, arise, artstate, exon, msl, vulture, tropyx, dream, excess, asphyxia, lamers, "
+	dc.b	"elude, amnesty, psl, speccy.pl, ghostown,  mhki, chaank, mystic, whelpz, implexy, addict, atz, scx, hup, genesis project, "
+	dc.b	"mwi, trsi, still, avatar, arsenic, asd, atlantis, bauknecht, booze design, bonzai, byterapers, camelot, censor design, "
+	dc.b	"chorus, cncd, cocoon, conspiracy, crest, delysid, extend, fairlight, farbrausch, fossil, hitmen, hoaxers, "
+	dc.b	"inque, lft, madteam, mercury, nuance, onslaught, oxyron, padua, plastic, plush, prosonix, razor1911, resource, "
+	dc.b	"role, shape, siesta, smash, titan, triad, tbl, unique, wrath designs, ...                          "
+	dc.b	'          '
+	dc.b	0
